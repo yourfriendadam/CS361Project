c4d1d6f306cee7cdad82606a21d16bdd8398c985
yourfriendadam <benckesa@oregonstate.edu>
2019-03-04 21:58:38 -0600

    updates db login information

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_benckesa',
-  password        : '5697',
-  database        : 'cs340_benckesa',
+  user            : 'cs361_benckesa',
+  password        : '****',
+  database        : 'cs361_benckesa',
   dateStrings     : 'date'
 });
 
